56ed4b330f3ebd6096e4440b52dc6da8901c5a54
OscarEpsilon <110500526+OscarEpsilon@users.noreply.github.com>
2023-06-22 18:29:41 -0400

    might maybe fix something
    a bunch of plugins files

---
+++ Source/The_Arena_5_1/The_Arena_5_1.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "The_Arena_5_1.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, The_Arena_5_1, "The_Arena_5_1" );
